135db454db631d510e1b30a226e2f0090973a80f
swostikpati <swostikpati@gmail.com>
2023-11-09 23:03:50 -0500

    handled prod bug

--- memory-threads-app/models/JournalEntryModel.js
+++ memory-threads-app/models/JournalEntryModel.js
@@ -1,16 +1,17 @@
 // JournalEntry.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const journalEntrySchema = new Schema({
   // user: { type: Schema.Types.ObjectId, ref: 'User'},
-  title: { type: String},
-  content: { type: String},
+  title: { type: String },
+  content: { type: String },
   specialLink: { type: String },
   image: { type: String },
   dateCreated: { type: Date, default: Date.now },
   lastUpdated: { type: Date, default: Date.now },
-  isArchived: { type: Boolean, default: false }
+  isArchived: { type: Boolean, default: false },
 });
 
-export default mongoose.models.JournalEntry || mongoose.model('JournalEntry', journalEntrySchema);;
+export default mongoose.models.JournalEntry ||
+  mongoose.model("JournalEntry", journalEntrySchema);
